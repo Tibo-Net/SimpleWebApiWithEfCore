22ddfb44cb58064e6fc63be33bfcf4d982d3704b
Tibo-Net <94740895+Tibo-Net@users.noreply.github.com>
2022-04-24 14:52:58 +0200

    Generate the required migration to be able to update (in this instance create) the database with the modification done to the DbContext and/Or the model by runing either: From the nugetPackage manager console in VS: Add-Migration CreatePersonsTable From a cmdline/powershell from the project folder path: dotnet ef migrations add CreatePersonsTable

---
+++ SimpleWebApiWithEfCore/Migrations/20220424125054_CreatePersonsTable.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SimpleWebApiWithEfCore.Migrations
+{
+    public partial class CreatePersonsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Persons");
+        }
+    }
+}
