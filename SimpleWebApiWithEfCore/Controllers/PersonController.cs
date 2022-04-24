e81375127555c0d853379b5f316d8c1fe69fe3bf
Tibo-Net <94740895+Tibo-Net@users.noreply.github.com>
2022-04-24 15:17:57 +0200

    Add a simple controller with  basic Add,Get,GetAll,Delete method

---
+++ SimpleWebApiWithEfCore/Controllers/PersonController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleWebApiWithEfCore.DataAccess;
+using SimpleWebApiWithEfCore.Model;
+
+namespace SimpleWebApiWithEfCore.Controllers;
+
+[ApiController]
+[Route("[controller]/[Action]")]
+public class PersonController : ControllerBase
+{
+    protected AppDbContext DbContext { get; private set; }
+
+    public PersonController()
+    {
+        DbContext = new AppDbContext();
+    }
+
+    [HttpPost]
+    public ActionResult<Person> Add(Person person)
+    {
+        DbContext.Add(person);
+        DbContext.SaveChanges();
+        return Ok(person);
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Person>> All()
+    {
+        return Ok(DbContext.Persons.ToList());
+    }
+
+    [HttpGet]
+    public ActionResult<Person> Get(int id) => DbContext.Persons.FirstOrDefault(p => p.Id == id);
+
+    [HttpDelete]
+    public ActionResult Delete(int id)
+    {
+        var person = DbContext.Persons.FirstOrDefault(x => x.Id == id);
+        if (person == null)
+        {
+            return NotFound();
+        }
+        DbContext.Persons.Remove(person);
+        DbContext.SaveChanges();
+        return Ok();
+    }
+}
