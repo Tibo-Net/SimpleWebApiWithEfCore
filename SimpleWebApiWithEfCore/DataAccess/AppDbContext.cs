a993c90b6d111432bafedb3f7f3f8bd22f81720b
Tibo-Net <94740895+Tibo-Net@users.noreply.github.com>
2022-04-24 14:43:24 +0200

    Add a DbContext with a Persons DbSet

---
+++ SimpleWebApiWithEfCore/DataAccess/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleWebApiWithEfCore.Model;
+
+namespace SimpleWebApiWithEfCore.DataAccess;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Person> Persons { get; set; }
+}
