e4f8c789e1cec41baf5a2dc9a3a6dc99526dc3fe
Tibo-Net <94740895+Tibo-Net@users.noreply.github.com>
2022-04-24 15:34:12 +0200

    Use string for enum in the  swagger

--- SimpleWebApiWithEfCore/Program.cs
+++ SimpleWebApiWithEfCore/Program.cs
@@ -1,11 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 using SimpleWebApiWithEfCore.DataAccess;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("AppDbConnectionString")));
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
