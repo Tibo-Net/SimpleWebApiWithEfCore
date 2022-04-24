1b04812f9c083842a2eaa68a0f4d87a1a8d1dbe5
Tibo-Net <94740895+Tibo-Net@users.noreply.github.com>
2022-04-24 14:54:59 +0200

    Limite the size of the string column to 50 using DataAnnotation

--- SimpleWebApiWithEfCore/Model/Person.cs
+++ SimpleWebApiWithEfCore/Model/Person.cs
@@ -1,8 +1,10 @@
-﻿namespace SimpleWebApiWithEfCore.Model;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleWebApiWithEfCore.Model;
 
 public class Person
 {
     public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+    [MaxLength(50)] public string FirstName { get; set; }
+    [MaxLength(50)] public string LastName { get; set; }
 }
