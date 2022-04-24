c175db1947c4aa8a6f4bb4d2c3f5f914d051154e
Tibo-Net <94740895+Tibo-Net@users.noreply.github.com>
2022-04-24 15:07:35 +0200

    Remove Unused DataAnnotation

--- SimpleWebApiWithEfCore/Model/Person.cs
+++ SimpleWebApiWithEfCore/Model/Person.cs
@@ -1,10 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace SimpleWebApiWithEfCore.Model;
+﻿namespace SimpleWebApiWithEfCore.Model;
 
 public class Person
 {
     public int Id { get; set; }
-    [MaxLength(50)] public string FirstName { get; set; }
-    [MaxLength(50)] public string LastName { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
 }
