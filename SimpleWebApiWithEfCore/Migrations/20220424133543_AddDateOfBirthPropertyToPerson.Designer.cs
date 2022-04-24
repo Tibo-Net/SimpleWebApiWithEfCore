82cc920064cf1f61a8047c3ad08df03eac0c45c4
Tibo-Net <94740895+Tibo-Net@users.noreply.github.com>
2022-04-24 15:38:36 +0200

    Add a DateOfBirth ForeignKey to a Date table to the person table.

--- SimpleWebApiWithEfCore/Migrations/20220424133543_AddDateOfBirthPropertyToPerson.Designer.cs
+++ SimpleWebApiWithEfCore/Migrations/20220424133543_AddDateOfBirthPropertyToPerson.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SimpleWebApiWithEfCore.DataAccess;
 
@@ -11,9 +12,10 @@ using SimpleWebApiWithEfCore.DataAccess;
 namespace SimpleWebApiWithEfCore.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220424133543_AddDateOfBirthPropertyToPerson")]
+    partial class AddDateOfBirthPropertyToPerson
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- SimpleWebApiWithEfCore/Model/Person.cs
+++ SimpleWebApiWithEfCore/Model/Person.cs
@@ -6,6 +6,15 @@ public class Person
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public Gender Gender { get; set; }
+    public Date DateOfBirth { get; set; }
+}
+
+public class Date
+{
+    public Guid Id { get; set; }
+    public int Year { get; set; }
+    public int Month { get; set; }
+    public int Day { get; set; }
 }
 
 public enum Gender
