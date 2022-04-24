133e063711e42de5a01234ac1f2cc64d23d779f8
Tibo-Net <94740895+Tibo-Net@users.noreply.github.com>
2022-04-24 14:45:35 +0200

    hard code temporary setup of EF for Sql Server using local db to be able to add a migration and update the database to create it.

--- SimpleWebApiWithEfCore/DataAccess/AppDbContext.cs
+++ SimpleWebApiWithEfCore/DataAccess/AppDbContext.cs
@@ -6,4 +6,10 @@ namespace SimpleWebApiWithEfCore.DataAccess;
 public class AppDbContext : DbContext
 {
     public DbSet<Person> Persons { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=(localdb)\\ProjectModels;Database=SimpleWebApiWithEfCore;Trusted_Connection=True;");
+        base.OnConfiguring(optionsBuilder);
+    }
 }
